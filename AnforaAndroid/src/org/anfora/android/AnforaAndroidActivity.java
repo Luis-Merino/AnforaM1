514cc63a904570cfe71138effe91438d6d06ed6c
Luis-Merino <luis.merino@anforasoftware.org>
2012-05-21 10:25:49 +0200

    Create initial project

---
+++ AnforaAndroid/src/org/anfora/android/AnforaAndroidActivity.java
@@ -0,0 +1,13 @@
+package org.anfora.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AnforaAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
